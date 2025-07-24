0993ac0dc4a37245a7156faf09f9733794eb7090
vicenteschwarz <vicenteschwarzmuller77@gmail.com>
2025-07-24 17:06:04 -0300

    Update preload.js

---
+++ preload.js
@@ -0,0 +1,7 @@
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('electronAPI', {
+  abrirHome: () => ipcRenderer.invoke('abrir-home'),
+  abrirAdmin: () => ipcRenderer.invoke('abrir-admin'),
+  abrirAgendamento: () => ipcRenderer.invoke('abrir-agendamento')
+});
