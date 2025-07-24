4c125363ba9d6a6e5ed8abff22ac780f1256ddd8
vicenteschwarz <vicenteschwarzmuller77@gmail.com>
2025-07-24 17:06:20 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,39 @@
-const { app, BrowserWindow } = require('electron');
+
+const { app, BrowserWindow, ipcMain } = require('electron');
 const path = require('path');
 
-function createMainWindow() {
+function createWindow(pagina = 'src/login/index.html') {
   const win = new BrowserWindow({
     width: 1200,
     height: 800,
     webPreferences: {
-      nodeIntegration: true,
-      contextIsolation: false
+      preload: path.join(__dirname, 'preload.js'),
+      contextIsolation: true,
+      nodeIntegration: false
     }
   });
-  win.loadFile("src/login/index.html");
+  win.loadFile(path.join(__dirname, pagina));
+  return win;
 }
 
-app.whenReady().then(createMainWindow);
+app.whenReady().then(() => {
+  createWindow();
 
+  ipcMain.handle('abrir-home', () => {
+  return createWindow('src/homePage/home.html');
+});
+
+ipcMain.handle('abrir-admin', () => {
+  return createWindow('src/adm/admin.html');
+});
+
+ipcMain.handle('abrir-agendamento', () => {
+  return createWindow('src/agendamento/agendamento.html');
+});
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
+
+//teste commit falho
